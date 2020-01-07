a28f2c693b1b4f1864b8a845becb8ae01a9f04ab
Håvard Molversmyr <havard.molversmyr@nmbu.no>
2020-01-07 13:03:12 +0100

    Added names and e-mails

---
+++ tests/test_animals.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+
+"""
+Test set for superclass Animals, and subclasses Herbivores and Carnivores.
+
+Test set for BioSim class interface for INF200 January 2019.
+
+This set of tests checks the interface of the BioSim class to be provided by
+the simulation module of the biosim package.
+
+Notes:
+     - The BioSim class should pass all tests in this set.
+     - The tests check only that the class interface can be used, not that
+       the class functions correctly. You need to write your own tests for that.
+     - You should only run these tests on your code *after* you have implemented
+       both animal and all landscape classes.
+"""
+
+__author__ = "Erik Rullestad", "Håvard Molversmyr"
+__email__ = "erikrull@nmbu.no", "havardmo@nmbu.no"
+
