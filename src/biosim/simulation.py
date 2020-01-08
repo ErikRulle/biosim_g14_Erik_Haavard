d31b71835f17034ee8da0261fad3b7fe8f2d23d8
Håvard Molversmyr <havard.molversmyr@nmbu.no>
2020-01-08 13:53:45 +0100

    fixed names and emails

--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -3,8 +3,8 @@
 """
 """
 
-__author__ = "Erik Rullestad, Håvard Molversmyr"
-__email__ = "erikrull@nmbu.no, havardmo@nmbu.no"
+__author__ = "Erik Rullestad", "Håvard Molversmyr"
+__email__ = "erikrull@nmbu.no", "havardmo@nmbu.no"
 
 
 class BioSim:
