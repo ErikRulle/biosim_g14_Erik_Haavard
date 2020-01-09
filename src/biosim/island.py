4bd8ac83391701bf0f80985eafdd6437683e5ed9
Håvard Molversmyr <havard.molversmyr@nmbu.no>
2020-01-09 10:30:53 +0100

    Created sphinx and changed Island class

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -8,33 +8,63 @@ class Island:
     """
     This class generates the island nation of Pylandia.
     """
-    def __init__(self):
+
+    STANDARD_MAP = """\
+                       OOOOOOOOOOOOOOOOOOOOO
+                       OOOOOOOOSMMMMJJJJJJJO
+                       OSSSSSJJJJMMJJJJJJJOO
+                       OSSSSSSSSSMMJJJJJJOOO
+                       OSSSSSJJJJJJJJJJJJOOO
+                       OSSSSSJJJDDJJJSJJJOOO
+                       OSSJJJJJDDDJJJSSSSOOO
+                       OOSSSSJJJDDJJJSOOOOOO
+                       OSSSJJJJJDDJJJJJJJOOO
+                       OSSSSJJJJDDJJJJOOOOOO
+                       OOSSSSJJJJJJJJOOOOOOO
+                       OOOSSSSJJJJJJJOOOOOOO
+                       OOOOOOOOOOOOOOOOOOOOO"""
+
+    def __init__(self, island_map=None):
         """
         This method creates variables needed for the class.
         """
-        pass
 
-    def map_of_pylandia(self, input_map=None):
-        """
-        This method creates the map.
-        :param input_map: map represented as a multi line string.
-        :return:
-        """
-        standard_map = """\
-               OOOOOOOOOOOOOOOOOOOOO
-               OOOOOOOOSMMMMJJJJJJJO
-               OSSSSSJJJJMMJJJJJJJOO
-               OSSSSSSSSSMMJJJJJJOOO
-               OSSSSSJJJJJJJJJJJJOOO
-               OSSSSSJJJDDJJJSJJJOOO
-               OSSJJJJJDDDJJJSSSSOOO
-               OOSSSSJJJDDJJJSOOOOOO
-               OSSSJJJJJDDJJJJJJJOOO
-               OSSSSJJJJDDJJJJOOOOOO
-               OOSSSSJJJJJJJJOOOOOOO
-               OOOSSSSJJJJJJJOOOOOOO
-               OOOOOOOOOOOOOOOOOOOOO"""
-        if input_map is None:
-            island_map = standard_map
+        if island_map is None:
+            self.island_map = self.STANDARD_MAP
         else:
-            island_map = input_map
+            self.island_map = island_map
+
+        self.validate_map_string(self.island_map)
+
+
+    def validate_map_string(self, island_map):
+        string_map = island_map.replace(" ", "").splitlines()
+        accepted_landscape_types = ["J", "S", "D", "M", "O"]
+        for row in string_map:
+            for cell in row:
+                if cell not in accepted_landscape_types:
+                    raise ValueError(
+                        "You have entered invalid landscape types.\n"
+                        "Please enter the following landscape types:\n"
+                        "J = Jungle\n"
+                        "S = Savannah\n"
+                        "D = Desert\n"
+                        "M = Mountain\n"
+                        "O = Ocean\n")
+
+        error_message = ValueError("The edges of the map has to be Ocean")
+        for end in [0, -1]:
+            for cell in string_map[end]:
+                if cell != "O":
+                    raise error_message
+        for row in string_map[1:-1]:
+            first_cell = row.startswith("O")
+            last_cell = row.endswith("O")
+            if not (first_cell and last_cell):
+                raise error_message
+
+        row_lengths = [len(row) for row in string_map]
+        for length in row_lengths:
+            if length != row_lengths[0]:
+                raise ValueError("All rows of the map must be of same length")
+
--- tests/test_island.py
+++ tests/test_island.py
@@ -14,35 +14,6 @@ def test_island_instance():
     assert isinstance(island, Island)
 
 
-def test_map_string(island_map):
-    string_map = island_map.replace(" ", "").splitlines()
-    accepted_landscape_types = ["J", "S", "D", "M", "O"]
-    for row in string_map:
-        for cell in row:
-            if cell not in accepted_landscape_types:
-                raise ValueError("You have entered invalid landscape types,"
-                                 "please enter the following landscape types:"
-                                 "J=Jungle"
-                                 "S=Savannah"
-                                 "D=Desert"
-                                 "M=Mountain"
-                                 "O=Ocean")
-
-    error_message = ValueError("The edges of the map has to be Ocean")
-    for end in [0, -1]:
-        for cell in string_map[end]:
-            if cell != "O":
-                raise error_message
-    for row in string_map[1:-1]:
-        first_cell = row.startswith("O")
-        last_cell = row.endswith("O")
-        if not first_cell and last_cell:
-            raise error_message
-
-    row_lengths = [len(row) for row in string_map]
-    for length in row_lengths:
-        assert length == len(string_map[0])
-
 
 
 
