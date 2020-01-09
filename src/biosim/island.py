502bce71dc5f007c42ef79a38807af70650e4f88
Erik Rullestad <erikrull@nmbu.no>
2020-01-09 11:58:27 +0100

    Made a numpy map for our island

--- src/biosim/island.py
+++ src/biosim/island.py
@@ -4,6 +4,9 @@ __author__ = "Erik Rullestad, Håvard Molversmyr"
 __email__ = "erikrull@nmbu.no, havardmo@nmbu.no"
 
 
+import numpy as np
+from biosim.landscape import *
+
 class Island:
     """
     This class generates the island nation of Pylandia.
@@ -34,13 +37,13 @@ class Island:
         else:
             self.island_map = island_map
 
-        self.validate_map_string(self.island_map)
+        self.string_map = island_map.replace(" ", "").splitlines()
+        self.validate_map_string()
 
 
-    def validate_map_string(self, island_map):
-        string_map = island_map.replace(" ", "").splitlines()
+    def validate_map_string(self):
         accepted_landscape_types = ["J", "S", "D", "M", "O"]
-        for row in string_map:
+        for row in self.string_map:
             for cell in row:
                 if cell not in accepted_landscape_types:
                     raise ValueError(
@@ -54,17 +57,37 @@ class Island:
 
         error_message = ValueError("The edges of the map has to be Ocean")
         for end in [0, -1]:
-            for cell in string_map[end]:
+            for cell in self.string_map[end]:
                 if cell != "O":
                     raise error_message
-        for row in string_map[1:-1]:
+        for row in self.string_map[1:-1]:
             first_cell = row.startswith("O")
             last_cell = row.endswith("O")
             if not (first_cell and last_cell):
                 raise error_message
 
-        row_lengths = [len(row) for row in string_map]
+        row_lengths = [len(row) for row in self.string_map]
         for length in row_lengths:
             if length != row_lengths[0]:
                 raise ValueError("All rows of the map must be of same length")
 
+    def landscape_position_in_map(self):
+        """
+        Creates the numpy array map
+        :return: numpy.ndarray, numpy map with landscape positions
+        """
+        self.numpy_map = np.empty(
+            (len(self.string_map), len(self.string_map[0])), dtype=object)
+        for x, line in enumerate(self.string_map):
+            for y, cell in enumerate(line):
+                if cell == "J":
+                    self.numpy_map[x, y] = Jungle()
+                elif cell == "S":
+                    self.numpy_map[x, y] = Savannah()
+                elif cell == "D":
+                    self.numpy_map[x, y] = Desert()
+                elif cell == "M":
+                    self.numpy_map[x, y] = Mountain()
+                elif cell == "O":
+                    self.numpy_map[x, y] = Ocean()
+        return self.numpy_map
--- tests/test_island.py
+++ tests/test_island.py
@@ -13,7 +13,14 @@ def test_island_instance():
     island = Island()
     assert isinstance(island, Island)
 
+def test_landscape_position_in_map():
+    """
 
+    :return:
+    """
+    island = Island()
+    island.landscape_position_in_map()
+    assert isinstance(island.numpy_map, numpy.ndarray)
 
 
 
